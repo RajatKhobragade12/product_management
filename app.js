f28b8604dbdb75f8e85ce46331adfdc0fd385b0d
Rajat Khobragade <rajat.khobragade@optimite.net>
2024-07-21 09:52:43 +0530

    dummy commit

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const routes = require('./src/routes/route');
+
 const app = express();
-mongoose.connect(process.env.DB)
 app.use(express.json());
+
+mongoose.connect(process.env.DB)
+
+
 const port = process.env.PORT
 
 app.use("/",routes)
